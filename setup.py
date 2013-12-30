5503db84739833dae952d2bde3d026898e05513e
Alexandr Kalenuk <akalenuk@gmail.com>
2013-12-30 17:29:56 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='Chalks',
+      version='1.0',
+      description='Code viewer',
+      author='Alexandr Kalenuk',
+      author_email='akalenuk@gmail.com',
+      license='Apache 2.0',
+      py_modules=['chalks'],
+     )
